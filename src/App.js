e7052c345f6a08e8e39edb21774994bf495a3584
babinicz <babinicz@onet.pl>
2022-02-01 18:54:31 +0100

    zmiany

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ import Laurels from "./container/Laurels/Laurels";
 import Gallery from "./container/Gallery/Gallery";
 import FindUs from "./container/Findus/FindUs";
 import Footer from "./container/Footer/Footer";
+import Scroll from "./Scroll";
+import ScrollToTop from "./ScrollToTop";
+import CookieConsent from "react-cookie-consent";
 
 const App = () => (
-  <div>
+  <>
+    <Scroll />
+    <ScrollToTop />
     <Navbar />
     <Header />
     <AboutUs />
@@ -23,7 +28,34 @@ const App = () => (
     <Gallery />
     <FindUs />
     <Footer />
-  </div>
+    <CookieConsent
+      debug={true}
+      location="bottom"
+      style={{
+        background: "#333",
+        textAlign: "left",
+        paddingBottom: "1rem",
+        fontSize: "16px",
+        fontFamily: "Gideon Roman",
+      }}
+      buttonStyle={{
+        color: "#333",
+        background: "#fff",
+        fontSize: "18px",
+        fontFamily: "Gideon Roman",
+        marginRight: "1rem",
+      }}
+      buttonText="OK, rozumiem"
+      expires={365}
+    >
+      "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+      Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+      indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+      dotyczących cookies oznacza, że będą one zamieszczane w Państwa urządzeniu
+      końcowym. Możecie Państwo dokonać w każdym czasie zmiany ustawień
+      dotyczących cookies."
+    </CookieConsent>
+  </>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
+  </Router>,
   document.getElementById("root")
 );
